4118b6625dcc137e66e4d3ea08f9382570f7b081
wasayr1 <wasayr1@gmail.com>
2025-07-28 01:05:34 -0400

    db context

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,12 +1,14 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SignalRMessenger.Models;
 
 namespace SignalRMessenger.Data;
 
-public class ApplicationDbContext : IdentityDbContext
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
     }
+    public DbSet<Message> Messages { get; set; }
 }

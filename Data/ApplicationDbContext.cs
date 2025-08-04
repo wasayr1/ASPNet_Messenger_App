feffc30f3d228f46ba82dd428d742ef5ed7bffd0
wasayr1 <wasayr1@gmail.com>
2025-08-04 19:33:13 -0400

    fixed db error

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -11,4 +11,22 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
     }
     public DbSet<Message> Messages { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+    base.OnModelCreating(builder); 
+
+    builder.Entity<Message>()
+        .HasOne(m => m.Sender)
+        .WithMany()
+        .HasForeignKey(m => m.SenderId)
+        .OnDelete(DeleteBehavior.Restrict); // Change cascade delete to restrict
+
+    builder.Entity<Message>()
+        .HasOne(m => m.Receiver)
+        .WithMany()
+        .HasForeignKey(m => m.ReceiverId)
+        .OnDelete(DeleteBehavior.Restrict); // Change cascade delete to restrict
+    }   
 }
+

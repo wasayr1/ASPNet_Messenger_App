96b0d0f6f867cea7bfcae3d97c1f8e9378b8f105
wasayr1 <wasayr1@gmail.com>
2025-08-05 23:01:14 -0400

    code to run db migrations automatically

--- Program.cs
+++ Program.cs
@@ -23,6 +23,22 @@ builder.Services.AddSignalR().AddHubOptions<ChatHub>(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<ApplicationDbContext>();
+        context.Database.Migrate();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while migrating the database.");
+    }
+}
+
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
